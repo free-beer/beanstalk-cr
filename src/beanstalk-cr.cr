46f5b837f25eb8a160c56ac5f04ecc81965958ce
Peter Wood <peter.wood@longboat.com>
2020-09-28 10:19:57 +0100

    Upped the internal library version number to match the shard version.

--- src/beanstalk-cr.cr
+++ src/beanstalk-cr.cr
@@ -4,7 +4,7 @@ require "./beanstalk"
 # TODO: Write documentation for `Beanstalk::Cr`
 module Beanstalk
   # Version constant.
-  VERSION = "0.1.0"
+  VERSION = "0.2.2"
 
   # Library logger.
   Log = ::Log.for("beanstalk-cr")
